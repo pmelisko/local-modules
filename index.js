09fd57da1bb6685f640d3b032b542bc9478e8b0d
Peter Melisko <pmelisko@gratex.com>
2017-10-31 10:52:40 +0100

    install.js updated, use folders as modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-let test = require("@my_module/test");
-let test2 = require("@my_module/test2");
+let test = require("test");
+let test2 = require("test2");
 
 console.log(test.greeting("world"));
 console.log(test2.greeting("world"));
--- scripts/install.js
+++ scripts/install.js
@@ -4,28 +4,30 @@ const path = require('path');
 console.log('Creating symlink to local modules');
 
 const localModulePaths = [
-	"@my_module"
+	"my_module/test",
+	"my_module/test2"
 ];
 const base = "./";
 
 localModulePaths.forEach((modulePath) => {
+	var moduleName = path.basename(modulePath);
 	// check if already installed (symlink already created)
-	if (fs.existsSync(`node_modules/${modulePath}`)) {
-		console.log(`Module ${modulePath} already exists`)
+	if (fs.existsSync(`node_modules/${moduleName}`)) {
+		console.log(`Module ${moduleName} already exists`)
 	} else {
 		let source = path.resolve(base, modulePath);
 		if (!fs.existsSync(source)) {
 			console.error(`${source} doesnt exist`);
 			process.exit(1); // error!
 		} else {
-			const linkPath = `node_modules/${modulePath}`;
+			const linkPath = `node_modules/${moduleName}`;
 			// ensure, node_modules folder will exists, if no dependencies, node_modules will not be created
 			if (!fs.existsSync('node_modules')){
 				fs.mkdirSync('node_modules');
 			}
 
 			fs.symlinkSync(source, linkPath, 'junction');
-			console.log(`Module ${modulePath} created`);
+			console.log(`Module ${moduleName} created`);
 		}
 	}
 });
